f046a002d89c4b83dabcac346edef8a6623a44cf
Pravhat Ray <pravhatray@gmail.com>
2022-10-28 12:32:44 +0530

    Add files via upload

---
+++ src/post/post.schema.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+const postSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    avatar:{type:String},
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true }
+},
+    {
+        versionKey: false,
+        timestamps: {
+            createdAt: 'created_at',
+            updatedAt: 'updated_at'
+        }
+    }
+)
+
+const Post = mongoose.model("post", postSchema)
+
+module.exports = Post;
