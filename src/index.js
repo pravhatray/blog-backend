f89e97aae1be3e8c08b3a809a3b9fb3a6bb3ab0a
pravhatray <pravhatray@gmail.com>
2022-10-28 18:09:35 +0530

    pushed

--- src/index.js
+++ src/index.js
@@ -23,5 +23,5 @@ app.get("/", (req, res) => {
 
 app.listen(8080, async () => {
     await Connect()
-    console.log(`Server is running on port ${PORT}`)
+    console.log(`Server is running on port 8080`)
 })
